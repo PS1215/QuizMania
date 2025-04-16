b4de329f8d82441f95f27de951e81e890ee5e796
ishaan khan <122585129+Ishaankhan7@users.noreply.github.com>
2025-04-16 14:15:57 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import io from 'socket.io-client';
 
- const socket = io("ws://localhost:5000");
+ const socket = io("ws://quizmania-34tu.onrender.com/");
 function App() {
   const [name, setName] = useState(null);
   const [room, setRoom] = useState(null);
